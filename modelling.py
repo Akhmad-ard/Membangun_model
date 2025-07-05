f5c0bfc07a0fe0a8df9d86e106eb0b73d4f9bfa1
akhmad-ardi <akhmad.ardi1711@gmail.com>
2025-07-06 00:27:50 +0800

    first commit

---
+++ modelling.py
@@ -0,0 +1,32 @@
+import mlflow
+import mlflow.sklearn
+import pandas as pd
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error, r2_score
+
+# Autolog semua parameter dan metrics
+mlflow.sklearn.autolog()
+
+# Load Dataset
+
+path = "./Predict_Student_Performance_preprocessing"
+
+df_train = pd.read_csv(f"{path}/train.csv")
+df_test = pd.read_csv(f"{path}/test.csv")
+
+X_train = df_train.drop(columns=['Grades'])
+y_train = df_train["Grades"]
+
+X_test = df_test.drop(columns=['Grades'])
+y_test = df_test["Grades"]
+
+with mlflow.start_run():
+    model = RandomForestRegressor()
+    model.fit(X_train, y_train)
+
+    predictions = model.predict(X_test)
+    mse = mean_squared_error(y_test, predictions)
+    r2 = r2_score(y_test, predictions)
+
+    print(f"Mean Squared Score: {mse}")
+    print(f"R-Squared Score: {r2}")
